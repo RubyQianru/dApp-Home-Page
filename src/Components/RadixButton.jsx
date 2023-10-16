ea28ec77c9350db8e1c8e644a48c8be23b0a221f
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2023-10-16 13:52:27 -0400

    Update RadixButton.jsx

--- src/Components/RadixButton.jsx
+++ src/Components/RadixButton.jsx
@@ -2,17 +2,29 @@ import React from 'react'
 import { RadixDappToolkit, RadixNetwork } from '@radixdlt/radix-dapp-toolkit'
 
 const rdt = RadixDappToolkit({
-  dAppDefinitionAddress:
-    'account_tdx_e_128uml7z6mqqqtm035t83alawc3jkvap9sxavecs35ud3ct20jxxuhl',
-  networkId: RadixNetwork.RCnetV3,
-  applicationName: 'Radix Web3 dApp',
-  applicationVersion: '1.0.0',
+    dAppDefinitionAddress:
+        'account_tdx_e_128uml7z6mqqqtm035t83alawc3jkvap9sxavecs35ud3ct20jxxuhl',
+    networkId: RadixNetwork.RCnetV3,
+    applicationName: 'Radix Web3 dApp',
+    applicationVersion: '1.0.0',
 })
 
 function RadixButton() {
-  return (
-    <radix-connect-button />
-  )
+    // rdt.walletApi.provideChallengeGenerator(requestChallengeFromDappBackendFn)
+
+    // rdt.walletApi.setRequestData(DataRequestBuilder.persona.withProof())
+
+    // // handle the wallet response
+    // rdt.walletApi.dataRequestControl(async (walletData) => {
+    // const personaProof = walletData.proofs.find(
+    //     (proof) => proof.type === 'persona'
+    // )
+    // if (personaProof) await handleLogin(personaProof)
+    // })
+  
+    return (
+      <radix-connect-button />
+    )
 }
 
 export default RadixButton
