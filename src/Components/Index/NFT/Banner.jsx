c14b4f46c0ce195aa8f760436220806ad9ee6dc9
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-05-02 19:11:42 -0400

    Update responsiveness

--- src/Components/Index/NFT/Banner.jsx
+++ src/Components/Index/NFT/Banner.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import LearnMore from "../LearnMore";
-import { Typography } from "@mui/material";
 import BlurryBlob from "../Decor/BlurryBlob";
 
 function Banner() {
@@ -8,9 +7,9 @@ function Banner() {
         <section className="w-screen h-screen flex justify-center items-center relative">
                 <BlurryBlob/>
                 <div className="w-1/2">
-                    <Typography variant="h2">
+                    <p className="bold text-3xl md:text-5xl">
                     Interact with your digital ownership in vritural 3D gallery.
-                    </Typography>
+                    </p>
                 </div>
             <LearnMore text="Go" color="black"/>  
         </section>
